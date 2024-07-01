49d8198ccdfd6946e98834371685886b5a92fe75
3nboyd <noahbboyd@gmail.com>
2024-07-01 14:26:54 -0500

    many visual changes, added videos, images

--- js/music.js
+++ js/music.js
@@ -6,62 +6,66 @@ $(document).ready(function() {
   // Generate content boxes dynamically
   let contentBoxes = [
     {
-        image: "js/content/tempgif.gif",
+        image: "js/content/showchoir.png",
         title: "Show Choir",
         date: "2/9/24",
         type: "Instagram",
         url: "https://www.instagram.com/p/C3IxqUwPgf0/" 
     },
     {
-        image: "js/content/tempimage.jpg",
+        image: "js/content/fiesta.png",
         title: "Fiesta!!!",
         date: "4/23/24",
         type: "Instagram",
-        url: "https://www.instagram.com/p/C2Tfqomu5hk/" 
+        url: "https://www.instagram.com/p/C6HlF7tuoGy/" 
     },
     {
-        image: "js/content/tempgif.gif",
+        image: "js/content/pepband.png",
         title: "Pep Band",
         date: "1/19/24",
         type: "Instagram",
         url: "https://www.instagram.com/showchoir" 
     },
     {
-        image: "js/content/tempimage.jpg",
-        title: "'I've Never Been In Love Before'",
+        image: "js/content/iveneverbeeninlovebefore.png",
+        title: "Newburgh Jazz Collective",
         date: "6/18/24",
         type: "Youtube",
         url: "https://www.youtube.com/watch?v=tWRsYMTp_2U&ab_channel=NewburghJazzCollective" 
     },
     {
-      image: "js/content/tempgif.gif",
+      image: "js/content/jamsessionvol4.png",
       title: "Jam Session vol. 4",
       date: "5/29/24",
       type: "Instagram",
       url: "https://www.instagram.com/p/C7kxhTGuG3R/",
   },
     {
-        image: "js/content/tempseemore.png",
-        title: "See More",
-        date: "",
-        type: "",
-        url: "contact.html" 
-    }
-];
+    //     image: "js/content/tempseemore.png",
+    //     title: "See More",
+    //     date: "",
+    //     type: "",
+    //     url: "contact.html" 
+    // }
+    // optional see more box that can prompt to whatever instagram or contact link
+}];
+
 
 $.each(contentBoxes, function(index, contentBox) {
-  let contentHTML = `
-    <a href="${contentBox.url}" target="_blank">
-      <div class="content-box">
-        <img src="${contentBox.image}" alt="${contentBox.title}">
-        <div class="content-text">
-          <p>${contentBox.title}</p>
-          <p>${contentBox.date} · ${contentBox.type}</p>
+  if (contentBox.image && contentBox.title && contentBox.date && contentBox.type && contentBox.url) {
+    let contentHTML = `
+      <a href="${contentBox.url}" target="_blank">
+        <div class="content-box">
+          <img src="${contentBox.image}" alt="${contentBox.title}">
+          <div class="content-text">
+            <p>${contentBox.title}</p>
+            <p>${contentBox.date} · ${contentBox.type}</p>
+          </div>
         </div>
-      </div>
-    </a>
-  `;
-  $(".content-grid").append(contentHTML);
+      </a>
+    `;
+    $(".content-grid").append(contentHTML);
+  }
 });
 });
 let upcomingEvents = [
@@ -74,8 +78,8 @@ let upcomingEvents = [
     url: "https://instagram.ford4-1.fna.fbcdn.net/v/t39.30808-6/435756498_18317400541131012_7313129143213855813_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDExNTIuc2RyLmYzMDgwOCJ9&_nc_ht=instagram.ford4-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=bF14iNulS10Q7kNvgFJA5to&edm=AEhyXUkAAAAA&ccb=7-5&ig_cache_key=MzMzOTE3NTgyMzYyMzg3NjU0Ng%3D%3D.2-ccb7-5&oh=00_AYDdTCc5KATWdNE6Z0T8IuTJUu8QMkSin8vWPpcn4o7TcA&oe=667F8F8E&_nc_sid="
   },
   {
-    image: "js/posters/Currents6.15",
-    url: "https://instagram.ford4-1.fna.fbcdn.net/v/t51.29350-15/448479233_441797898612968_5860568588591782701_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTQ5eDExNDYuc2RyLmYyOTM1MCJ9&_nc_ht=instagram.ford4-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=nXcuMMFgHMoQ7kNvgELAJDo&edm=AEhyXUkBAAAA&ccb=7-5&ig_cache_key=MzM5MDczMTM0MTIyODc2ODg1Nw%3D%3D.2-ccb7-5&oh=00_AYCt5X88_zfhnSkIUkvL0Kzyl9ojbo76OP_Bx4L0i3ocbA&oe=667FA40D&_nc_sid=8f1549"
+    image: "js/posters/Currents6.15.jpg",
+    url: "https://instagram.ford4-1.fna.fbcdn.net/v/t51.29350-15/448479233_441797898612968_5860568588591782701_n.jpg?stp=dst-jpg_e35_s720x720&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTQ5eDExNDYuc2RyLmYyOTM1MCJ9&_nc_ht=instagram.ford4-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=NvTCUkEfJcMQ7kNvgFPvUOc&edm=AEhyXUkBAAAA&ccb=7-5&ig_cache_key=MzM5MDczMTM0MTIyODc2ODg1Nw%3D%3D.2-ccb7-5&oh=00_AYCHrgkvgZAl1iUwAYnQHHjpU5KXWtSNh3cF69hmgdjxCQ&oe=6688DE8D&_nc_sid=8f1549"
   },
   {
     image: "js/posters/KnightofJazz4.13.24.jpg",
@@ -90,7 +94,7 @@ let upcomingEvents = [
 $.each(upcomingEvents, function(index, event) {
   let eventHTML = "";
   if (index < 5) {
-    eventHTML = `<a href="${event.url}" class="event-poster">
+    eventHTML = `<a href="${event.url}" target="_blank" class="event-poster">
       <img src="${event.image}" alt="Poster ${index + 1}">
     </a>`;
   } else {
